e38b5b8f2772aeaff5b4989f0251f8d837d09ee3
Jonathan Eduardo García García <jgarciag1404@alumno.ipn.mx>
2023-09-09 19:52:38 -0600

    Add docker linux

---
+++ web/jslt-vuetify/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main>
+      <MainView/>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  import MainView from '@/components/MainView.vue'
+</script>
---
+++ web/jslt-vuetify/src/components/MainView.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        expand-on-hover
+        rail
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            title="Sandra Adams"
+            subtitle="sandra_a88@gmailcom"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list v-model:selected="selectedTab" density="compact" nav>
+          <v-list-item prepend-icon="mdi-folder" title="Tests" value="tests"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 250px">
+        <v-card-text>
+          {{ selectedTab }}
+          The navigation drawer will appear from the bottom on smaller size screens.
+        </v-card-text>
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+  //
+  const selectedTab = ref(['tests'])
+
+</script>
