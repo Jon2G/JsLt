a474bfff4e5a3dba442a75578f97ecca6f64cae7
Jon2G <jonyjouna@gmail.com>
2024-01-01 03:47:46 -0600

    Update main.ts

--- aws/cdktf/main.ts
+++ aws/cdktf/main.ts
@@ -144,7 +144,7 @@ You can read more about this at https://cdk.tf/variables*/
       });
       const awsSubnetPublicSubnet2 = new aws.subnet.Subnet(this, "public-subnet2", {
         availabilityZone: awsBz,
-        cidrBlock: publicSubnetCidr,
+        cidrBlock: "10.1.104.0/24",
         vpcId: vpc.vpcIdOutput
       });
     /*This allows the Terraform resource name to match the original name. You can remove the call if you don't need them to match.*/
