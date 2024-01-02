bc343ead2471d4ba1f87310cc9e88e2295cf4ec9
Jon2G <jonyjouna@gmail.com>
2024-01-02 01:16:09 -0600

    add other functions

---
+++ aws/lambda/functions/execute/index.ts
@@ -0,0 +1,17 @@
+import {
+  Context,
+  APIGatewayProxyResult,
+  APIGatewayEvent,
+} from "aws-lambda";
+export async function handler(
+  event: APIGatewayEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> {
+  await Promise.resolve();
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "execute",
+    }),
+  };
+}
